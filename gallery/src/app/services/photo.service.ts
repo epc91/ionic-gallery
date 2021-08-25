bf70aa0fa62d0f15fe96761a02d61b3c1056c5cd
Eduardo Peschke <eduardopeschke@gmail.com>
2021-08-25 16:35:31 -0400

    Camera Working

---
+++ gallery/src/app/services/photo.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+
+// Custom Import
+import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+import { Filesystem, Directory } from '@capacitor/filesystem';
+import { Storage } from '@capacitor/storage';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotoService {
+
+  constructor() { }
+
+  // Methods
+  public async addNewToGallery() {
+    const capturedPhoto = await Camera.getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 100,
+    })
+  }
+}
