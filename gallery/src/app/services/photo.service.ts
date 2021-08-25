9f14f6a10911e56982bf3ca6e05b15d9ecbadb5e
Eduardo Peschke <eduardopeschke@gmail.com>
2021-08-25 17:25:39 -0400

    Add Methods To Photo Service

--- gallery/src/app/services/photo.service.ts
+++ gallery/src/app/services/photo.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 
 // Custom Import
-import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { Camera, CameraPhoto, CameraResultType, CameraSource } from '@capacitor/camera';
 import { Filesystem, Directory } from '@capacitor/filesystem';
 import { Storage } from '@capacitor/storage';
 
@@ -15,17 +15,46 @@ export class PhotoService {
 
   // Methods
   public async addNewToGallery() {
+    // Take Photo
     const capturedPhoto = await Camera.getPhoto({
       resultType: CameraResultType.Uri,
       source: CameraSource.Camera,
       quality: 100,
     });
 
-    this.photos.unshift({
-      filepath: "soon...",
-      webviewPath: capturedPhoto.webPath
+    // Save Picture
+    const savedImageFile = await this.savePicture(capturedPhoto);
+
+    this.photos.unshift(savedImageFile);
+  };
+  private async savePicture(cameraPhoto: CameraPhoto){
+    const base64Data = await this.readAsBase64(cameraPhoto);
+
+    const fileName = new Date().getTime() + '.jpeg'
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: Directory.Data
     });
-  }
+    return {
+      filepath: fileName,
+      webviewPath: cameraPhoto.webPath
+    }
+  };
+  private async readAsBase64(cameraPhoto: CameraPhoto){
+    const response = await fetch(cameraPhoto.webPath!);
+    const blob = await response.blob();
+
+    return await this.convertBlobToBase64(blob) as string;
+  };
+  convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
+    const reader = new FileReader;
+    reader.onerror = reject;
+    reader.onload = () => {
+      resolve(reader.result);
+    };
+    reader.readAsDataURL(blob);
+  })
 };
 
 export interface Photo {
